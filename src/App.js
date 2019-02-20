861d209c08fa268f27f468d3f05b54b72a0a35eb
DESKTOP-7BTGJ2T\Horger <mh3294@drexel.edu>
2019-02-19 19:57:47 -0500

    push react app changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  showAlert(report){
+    console.log(report);
   }
+
+  render(){
+        return(
+            <header className="mdl-layout__header mdl-layout__header--scroll mdl-color--grey-200">
+            <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title mdl-layout--large-screen-only" id="title_screen" style={{color: 'black'}}> SEPTA UI - DEV: V2.0.0.0 </span>
+                <nav className="mdl-navigation">
+                    <button id="reports-menu-lower-left" className="mdl-button mdl-js-button">
+                      Reports
+                    </button>
+                    <ul className="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect"
+                        htmlFor="reports-menu-lower-left">
+                      <li className="mdl-menu__item" onClick={() => {this.showAlert("")}}>Trains</li>
+                      <li className="mdl-menu__item" onClick={() => {this.showAlert("")}}>Subway</li>
+                      <li className="mdl-menu__item" onClick={() => {this.showAlert("")}}>Trolley</li>
+                      <li className="mdl-menu__item" onClick={() => {this.showAlert("")}}>Buses</li>
+                    </ul>
+
+                    <button id="maintenance-menu-lower-left" className="mdl-button mdl-js-button mdl-color-text--black">
+                      Maintenance
+                    </button>
+                    <ul className="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect"
+                        htmlFor="maintenance-menu-lower-left">
+                      <li className="mdl-menu__item" onClick={() => {this.showAlert("")}}>MyView</li>
+                      <li className="mdl-menu__item" onClick={() => {this.showAlert("")}}>TransitView</li>
+                      <li className="mdl-menu__item" onClick={() => {this.showAlert("")}}>Bus Detours</li>
+                      <li className="mdl-menu__item" onClick={() => {this.showAlert("")}}>System Alerts</li>
+                      <li className="mdl-menu__item" onClick={() => {this.showAlert("")}}>Elevator Outages</li>
+                    </ul>
+                </nav>
+            </div>
+            </header>
+          );
+        }
 }
 
 export default App;
