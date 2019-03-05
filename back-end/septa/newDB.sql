b8cbb361d4273486f5746434d62c96be6ad1b1f3
DESKTOP-7BTGJ2T\Horger <mh3294@drexel.edu>
2019-03-05 17:57:43 -0500

    Create newDB.sql
    Create new DB Schemas on septa 01 - 04

---
+++ back-end/septa/newDB.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE Regional_Rail
+	CHARACTER SET latin1
+	COLLATE latin1_swedish_ci;
+
+  CREATE DATABASE Light_Rail
+	CHARACTER SET latin1
+	COLLATE latin1_swedish_ci;
+
+  CREATE DATABASE Trolley
+	CHARACTER SET latin1
+	COLLATE latin1_swedish_ci;
+
+  CREATE DATABASE Bus
+	CHARACTER SET latin1
+	COLLATE latin1_swedish_ci;
