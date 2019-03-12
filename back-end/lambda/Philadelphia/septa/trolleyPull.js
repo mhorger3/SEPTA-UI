4ef233192c0f682975215a7ec6fc990097d3e52f
DESKTOP-7BTGJ2T\Horger <mh3294@drexel.edu>
2019-03-12 17:49:49 -0400

    Push parsing data initial

--- back-end/lambda/Philadelphia/septa/trolleyPull.js
+++ back-end/lambda/Philadelphia/septa/trolleyPull.js
@@ -1,4 +1,4 @@
-// TransitViewWeather
+// TransitViewTrolley
 // Matthew Horger
 // Drexel University
 // Utilizes usage of Apache License, Version 2.0, found here -  http://aws.amazon.com/apache2.0/
@@ -9,7 +9,7 @@ var mysql = require('mysql');
 
 exports.handler = function (intent, session, callback) {
     var options = {
-    host: 'www3.septa.org', // connect to SEPTA API
+    host: 'www3.septa.org', // connect to quizlet API
     port: 443,
     path: '/hackathon/TransitView/10',
     method: 'GET'
@@ -21,11 +21,47 @@ exports.handler = function (intent, session, callback) {
             body += chunk;
         });
         res.on('end', function() {
-            body = JSON.parse(body);
-            callback(null, body);
+            body = JSON.parse(body); //
+            for (var trolley in body.bus)
+            {
+               console.log(body.bus[trolley].trip);
+            }
+
+           /* var con = mysql.createConnection({
+              host: "septa01.ccgbnu8qerao.us-east-1.rds.amazonaws.com",
+              user: "lambda",
+              password: "lambda",
+              database: "Trolley"
+            });
+
+            con.connect(function(err) {
+              if (err) throw err;
+              console.log("Connected!");
+            });
+
+            var sql = "INSERT INTO ROUTE_10 VALUES('"
+                + body.weather[0].main +"','"+
+                  body.weather[0].description +"',"+
+                  body.rain["1h"] +","+
+                  body.snow["1h"] +","+
+                  body.main.temp +","+
+                  body.main.pressure +","+
+                  body.main.humidity +","+
+                  body.visibility +","+
+                  body.wind.speed +","+
+                  body.wind.gust +","+
+                  body.clouds.all +", CONVERT_TZ(Now(), 'GMT', 'EST'));";
+            console.log(sql);
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log("Result: " + result);
+            });
+            con.end();
+            */
         });
     });
     req.end(); // actually executes our code
 
+    callback(null, body);
 
 };
